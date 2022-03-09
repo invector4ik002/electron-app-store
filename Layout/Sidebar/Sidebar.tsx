b48bb63d51198729d5b605eba6cdedc13118b616
Девиченский Михаил <Vedoone@mail.ru>
2022-03-09 07:09:51 +0700

    refinement of the search component

--- Layout/Sidebar/Sidebar.tsx
+++ Layout/Sidebar/Sidebar.tsx
@@ -3,13 +3,14 @@ import style from './Sidebar.module.css';
 import cn from 'classnames';
 import { Menu } from '../Menu/Menu';
 import Logo from '../../Layout/logo.svg'
+import { Search } from '../../components';
 
 export const Sidebar = ({className, ...props }: SidebarProps): JSX.Element => {
 
 	return (
 		<div className={cn(className, style.sidebar)} {...props}>
 			<Logo className={style.logo}/>
-			Поиск
+			<Search />
 			<Menu />
 		</div>
 	)
--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { SearchProps } from './Search.props';
 import style from './Search.module.css';
 import cn from 'classnames';
+
+import { SearchProps } from './Search.props';
 import { Input } from '../Input/Input';
 import { Button } from '../Button/Button';
+import GlassIcon from './glass.svg';
 
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
 
 	const [search, setSearch] = React.useState<string>('');
 	console.log('search :>> ', search);
 	return (
-		<form className={cn(className, style.input)}>
+		<form className={cn(className, style.search)}>
 			<Input
+				className={style.input}
 				placeholder='Поиск...'
 				value={search}
 				onChange={(e) => setSearch(e.target.value)}
 			/>
+			<Button
+				className={style.button}
+				appearance='primary'
+				onClick={() => { }}
+			>
+				<GlassIcon />
+			</Button>
 		</form>
 	)
 }

7723250059e33005f96eac0efd748a76dd40fdf1
Девиченский Михаил <Vedoone@mail.ru>
2022-03-09 21:45:17 +0700

    поиск добавлен функционал клик и ентер

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -6,13 +6,32 @@ import { SearchProps } from './Search.props';
 import { Input } from '../Input/Input';
 import { Button } from '../Button/Button';
 import GlassIcon from './glass.svg';
+import { useRouter } from 'next/router';
 
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
 
 	const [search, setSearch] = React.useState<string>('');
-	console.log('search :>> ', search);
+
+	const router = useRouter();
+
+	const goToSearch =(e: any)=> {
+		e.preventDefault();
+		router.push({
+			pathname: '/search',
+			query: {
+				q: search
+			}
+		})
+	}
+
+	const handleKeyDown =(e: KeyboardEvent)=> {
+		if(e.key == 'Enter') {
+			goToSearch(e);
+		}
+	}
+
 	return (
-		<form className={cn(className, style.search)}>
+		<form className={cn(className, style.search)} {...props}>
 			<Input
 				className={style.input}
 				placeholder='Поиск...'
@@ -22,7 +41,8 @@ export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
 			<Button
 				className={style.button}
 				appearance='primary'
-				onClick={() => { }}
+				onClick={goToSearch}
+				onKeyDown={handleKeyDown}
 			>
 				<GlassIcon />
 			</Button>

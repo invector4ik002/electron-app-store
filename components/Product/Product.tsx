3e6aaeece070abc3bc76f459a7a9e827f36a66d9
Девиченский Михаил <Vedoone@mail.ru>
2022-03-13 23:15:27 +0700

    Добавлен компонент Divider и доделан компонент Card

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -6,6 +6,7 @@ import { priceRu } from '../../helpers/helpers';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
+import { Divider } from '../Divider/Divider';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	console.log('product :>> ', product);
@@ -13,14 +14,19 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 		<Card className={style.product}>
 			<div className={style.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
 			<div className={style.title}>{product.title}</div>
-			<div className={style.price}>{priceRu(product.price)}</div>
-			<div className={style.credit}>{priceRu(product.credit)}</div>
+			<div className={style.price}>
+				{priceRu(product.price)}
+				<Tag size={'s'} color={'green'} className={style.oldPrice}>{priceRu(product.price - product.oldPrice)}</Tag>
+			</div>
+			<div className={style.credit}>
+				{priceRu(product.credit)}/<span className={style.month}>мес</span>
+			</div>
 			<div className={style.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
 			<div className={style.tags}>{product.categories.map(c => <Tag key={c} color="ghost">{c}</Tag>)}</div>
 			<div className={style.priceTitle}>цена</div>
 			<div className={style.creditTitle}>кредит</div>
 			<div className={style.rateTitle}>{product.reviewCount} отзывов</div>
-			<div className={style.hr}><hr className={style.hr}/></div>
+			<Divider className={style.hr} />
 			<div className={style.description}>{product.description}</div>
 			<div className={style.feature}>фичи</div>
 			<div className={style.advBlock}>
@@ -33,7 +39,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 					<div>{product.disAdvantages}</div>
 				</div>
 			</div>
-			<div className={style.hr}><hr className={style.hr}/></div>
+			<Divider className={style.hr} />
 			<div className={style.action}>
 				<Button appearance={'primary'}>Узнать подробнее</Button>
 				<Button appearance={'ghost'} arrow={'right'}>Читать отзывы</Button>

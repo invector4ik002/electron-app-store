5eb3e49f1d3d2998d2c2d8067655ba392f789d0d
invector4ik002 <vedoone@mail.ru>
2022-06-03 15:52:49 +0700

    подключил яндекс метрику 'react-yandex-metrika'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
 import '../styles/globals.css';
+import React from 'react';
+import ym, { YMInitializer } from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
-  <>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
-    <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
-    <meta property='og:locale' content='ru_RU'/>
-  </>
-  return <Component {...pageProps} />
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  })
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+        <meta property='og:locale' content='ru_RU' />
+      </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version='2'
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
